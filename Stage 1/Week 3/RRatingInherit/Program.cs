b9209fc3278ecb78573f0e6a358fb5db66455ee7
Mark Kenne <mkenne@lincoln.ne.gov>
2023-08-02 16:02:10 -0500

    Rest Class only

--- Stage 1/Week 3/RRatingInherit/Program.cs
+++ Stage 1/Week 3/RRatingInherit/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿﻿using System;
+
+namespace restaurantAPP
+{
+  class Program
+  {
+    static void Main(string[] args)
+    { 
+      Restaurant R1 = new Restaurant();
+      Restaurant R2 = new Restaurant("Misty's", 4);
+      Console.WriteLine(R1);  // test Rest Class defaullts
+      Console.WriteLine(R2);  // test Rest w two variables
+      Console.WriteLine();
+
+    }
+  }
+}
