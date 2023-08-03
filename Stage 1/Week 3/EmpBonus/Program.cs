395812e00a4c4ee0fab7cc4c903b0d28d94c9486
Mark Kenne <mkenne@lincoln.ne.gov>
2023-08-03 13:23:00 -0500

    Array set up

--- Stage 1/Week 3/EmpBonus/Program.cs
+++ Stage 1/Week 3/EmpBonus/Program.cs
@@ -1,4 +1,27 @@
-﻿using System;
+﻿/* Employee Bonus Calculator
+   Programmer: Mark Kenne
+   Last Update:  08/03/2023
+   Program Description -  Demonstrate use of Class inheritance and polymorphism using a parent class of Employee and two child Classes (Hourly and Salary).
+  
+   Requirements:                      
+  (1) An array of Employees with a length of 25
+  (2) Array holds values of Employee's first name, last name, and Pay Type in each case and a pay rate or amount based on PayType
+  (3) Ability to calculate the annual bonus based on the Type. If H then 80 hours times rate, if Salary then 10% of salary amount
+  (4) Ability to print the hourly rate or salary amount with bonus using the ToString method
+  (5) A menu to operate the CRUD method (Create, Read, Update, and Delete) in regards to the array.
+  (6) A menu item Q to exit the program
+  (7) A procedure to Create a new row in the array
+  (8) A procedure to Read all rows in the array and print them without showing null rows
+  (9) A procedure to Update a row in the array
+  (10) A procedure to Delete a row in the array
+  (11) A procedure to Initialize the array and create all rows with null values 
+  (12) Optional procedure to Load the data array from a text file
+  (13) Optional procedure to Save the data array to a text file
+
+
+
+*/
+using System;
 
 namespace EmployeeApp
 {
@@ -6,6 +29,8 @@ namespace EmployeeApp
   {
     static void Main(string[] args)
     { 
+    
+// Test the three Classes - Employee parent and child Hourly and child Salary     
       Console.WriteLine();
       Employee Emp1 = new Employee();  //default properties test Employee
       Employee Emp2 = new Employee("Mark", "Kenne", "S"); //3 input properties test FName, LName, PayType
@@ -24,6 +49,41 @@ namespace EmployeeApp
       Console.WriteLine(Emp7);  // test Salary Class defaullts
      
       Console.WriteLine();
+
+        // Declare and instantiate the array of Restaurant objects
+        Employee[] EmpArray=new Employee[25];
+// Create an array of employees and initialize
+        for (int index = 0; index < EmpArray.Length; index++)
+        {
+            EmpArray[index] = new Employee();
+        }
+
+        // Load in some test data to test both ways to assign values
+
+        EmpArray[1] = Emp1;
+        EmpArray[2] = Emp2;
+        EmpArray[3] = Emp3;
+        EmpArray[4] = Emp4;
+        EmpArray[5] = Emp5;
+        EmpArray[6] = Emp6;
+
+
+        // print each Employee to test the property gets and the toString
+
+        for (int index = 0; index < EmpArray.Length; index++)
+        {
+            if (!((EmpArray[index].LName) == null))
+            {
+
+             Console.WriteLine(EmpArray[index]);   
+            }
+                
+        }
+
+
+
+
+
 
     }
   }
