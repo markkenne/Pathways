29f78cc47e1cf9098ee15adb44f0378cfca9d3db
Mark Kenne <mkenne@lincoln.ne.gov>
2023-08-08 10:30:55 -0500

    C and R from menu

--- Stage 1/Week 4/EmpBonusList/Program.cs
+++ Stage 1/Week 4/EmpBonusList/Program.cs
@@ -8,6 +8,10 @@ namespace listAbstractClass
     static void Main(string[] args)
     {
 
+        // Create a list of Employees
+        List<Employee> employeeList = new List<Employee>();
+
+
       List<string> menuOptions = new List<string>();
    
       menuOptions.Add("C - Create");
@@ -34,30 +38,61 @@ string menuchoiceUp = menuchoice.ToUpper();
 // direct the program to the menu choice using switch/case
 switch (menuchoiceUp)
 {
-  case "C":
+  case "C":  // TODO CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
     // Create a new item.
      Console.WriteLine("In the C/c area");
+        // Create a list of Employees
+      //  List<Employee> employeeList = new List<Employee>();
+        // Add an hourly employee
+        employeeList.Add(new HourlyEmployee("Enge", "J.S.", "H", 25.00));
+        // Add a salary employee
+        employeeList.Add(new SalaryEmployee("Enge", "Alec", "S", 15000));
+        // Add an employee? - Nope
+        // employeeList.Add(new Employee("Test", "test", "test"));
+
+        // Create a list of Hourly Employees
+        List<HourlyEmployee> hourlyEmployeeList = new List<HourlyEmployee>();  
+        // And, create a list of Salary Employees to test
+        List<SalaryEmployee> salaryEmployeeList = new List<SalaryEmployee>();  
+
+        // Add a couple of hourly employees to the hourly list to test
+        hourlyEmployeeList.Add(new HourlyEmployee ("Enge", "Colin", "H", 15.75));
+        hourlyEmployeeList.Add(new HourlyEmployee ("Enge", "Elle", "H", 25.73));
+
+        // Add a couple of salary employees to the salary list to test
+        salaryEmployeeList.Add(new SalaryEmployee ("Enge", "Anthony", "S", 45250));
+        salaryEmployeeList.Add(new SalaryEmployee ("Enge", "Molly", "S", 60000));
+
+
     break;
-  case "R":
+  case "R":  // TODO RRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR
     // Read an existing item.
     Console.WriteLine("In the R/r area");
+        // Print the employee list
+
+        foreach (Employee anEmployee in employeeList)
+            {
+                Console.WriteLine(anEmployee);
+            }  // end foreach      
+
     break;
-  case "U":
+  case "U":  // TODO UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU
     // Update an existing item.
      Console.WriteLine("In the U/u area");
     break;
-  case "D":
+  case "D": // TODO DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD
     // Delete an existing item.
      Console.WriteLine("In the D/d area");
     break;
-  case "Q":
+  case "Q": // TODO QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ
     // Quit the application.
     Console.WriteLine("In the Q/q area");
     isMenuLooping = false; // user chose Q so quit the looping
-    Console.WriteLine("Good Bye...");
+    Console.WriteLine("You chose Quit. Good Bye...");
     break;
   default:
-    // Invalid option.
+    // Invalid option. Loop to menu choices.
+    Console.WriteLine("Invalid Option. Please try again.");
     break;
 
 } 
