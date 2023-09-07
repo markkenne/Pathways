e8e473071d704c1c72a3f10a0fbabb7543cfaffc
Mark Kenne <mkenne@lincoln.ne.gov>
2023-09-07 09:01:44 -0500

    InterfaceAndDI2 created and working

---
+++ Stage 2/Week 3/InterfaceAndDI/InterfaceAndDI/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace InterfaceAndDI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILogger loggerFile = new CloudLogger();
+            ProductService productService1 = new ProductService(loggerFile);
+            productService1.Log("New log entry.");
+
+        }
+    }
+}
---
+++ Stage 2/Week 3/InterfaceAndDI2/InterfaceAndDI2/USBLogger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceAndDI
+{
+    public class USBLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine("Inside Log method of USBLogger.");
+            LogToUSB(message);
+        }
+        private void LogToUSB(string message)
+        {
+            Console.WriteLine("Method: LogToUSB, Text: {0}", message);
+        }
+    }
+}
