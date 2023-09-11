f9d6d240181816d09cf2e35d0b520834255deaa2
Mark Kenne <mkenne@lincoln.ne.gov>
2023-09-11 09:33:44 -0500

    Housebuilder edits

---
+++ Stage 2/Week 3/HouseBuilder/HouseBuilderTests/DoorTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HouseBuilder;
+//using System;
+
+
+
+namespace HouseBuilder.Tests
+{
+    [TestClass]
+    public class DoorTests
+    {
+        [TestMethod]
+        public void GetCost_ShouldReturnCorrectCost()
+        {
+            // Arrange
+            Door door = new Door(2, 200);
+
+            // Act
+            double cost = door.GetCost();
+
+            // Assert
+            Assert.AreEqual(400, cost);
+        }
+    }
+}
