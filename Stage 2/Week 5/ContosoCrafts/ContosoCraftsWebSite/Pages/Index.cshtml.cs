426183b10d2a6aeb87f5edd3360309d208a033b5
Mark Kenne <mkenne@lincoln.ne.gov>
2023-09-20 14:14:21 -0500

    Contoso v2

--- Stage 2/Week 5/ContosoCrafts/ContosoCraftsWebSite/Pages/Index.cshtml.cs
+++ Stage 2/Week 5/ContosoCrafts/ContosoCraftsWebSite/Pages/Index.cshtml.cs
@@ -1,25 +1,29 @@
-﻿using System.Collections.Generic;
-using ContosoCraftsWebSite.Models;
+﻿using ContosoCraftsWebSite.Models;
 using ContosoCraftsWebSite.Services;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 
 namespace ContosoCraftsWebSite.Pages
 {
+ 
+    
+    
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public JsonFileProductService ProductService;
+    
+        public IEnumerable<Product> Products { get; private set; }
 
-        public IndexModel(ILogger<IndexModel> logger,
+        public IndexModel(
+            ILogger<IndexModel> logger,
             JsonFileProductService productService)
         {
             _logger = logger;
             ProductService = productService;
         }
 
-        public JsonFileProductService ProductService { get; }
-        public IEnumerable<Product>? Products { get; private set; }
-
         public void OnGet() => Products = ProductService.GetProducts();
     }
 }
+
+
